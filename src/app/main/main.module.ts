86843d67027ed5c80ad833ea2767ceea4845a977
CheeSerj <chibisovse@yandex.ru>
2022-09-13 23:31:37 +0300

    Refactoring

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -5,10 +5,20 @@ import {AppModule} from "../app.module";
 import {MatIconModule} from "@angular/material/icon";
 import {MatStepperModule} from "@angular/material/stepper";
 import {MatButtonModule} from "@angular/material/button";
+import { ContactsComponent } from './cv/side-panel/contacts/contacts.component';
+import { KnowledgeComponent } from './cv/side-panel/knowledge/knowledge.component';
+import { EducationComponent } from './cv/side-panel/education/education.component';
+import { HobbiesComponent } from './cv/side-panel/hobbies/hobbies.component';
+import { StepperComponent } from './cv/content/stepper/stepper.component';
 
 @NgModule({
   declarations: [
-    CvComponent
+    CvComponent,
+    ContactsComponent,
+    KnowledgeComponent,
+    EducationComponent,
+    HobbiesComponent,
+    StepperComponent
   ],
   imports: [
     CommonModule,
