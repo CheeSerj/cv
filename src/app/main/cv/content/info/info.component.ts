f15145bb9591fe9565de874fba6cced2d1467648
CheeSerj <chibisovse@yandex.ru>
2022-09-18 00:19:57 +0300

    added moment lib

--- src/app/main/cv/content/info/info.component.ts
+++ src/app/main/cv/content/info/info.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import * as moment from "moment";
+
+const dateNow = moment([]) ;
+const startWork = moment([2022, 0, 11]);
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
   styleUrls: ['./info.component.scss']
 })
-export class InfoComponent implements OnInit {
+export class InfoComponent {
 
-  constructor() { }
-
-  public ngOnInit(): void {
-  }
-
+  public countDaysWork = dateNow.diff(startWork, 'months')
 }
